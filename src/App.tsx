d7e7ee3ce8382424fdd8dc9d7a3cd4039a949da1
trankhanhbao <tranbao22051999@gmail.com>
2024-03-14 17:25:18 +0700

    xong video #31

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Portfolio from '@/screens/portfolio'
+import '@/style/plugins.scss'
+import '@/style/style.scss'
+
+function App() {
+  return (
+    <Portfolio />
+  )
+}
+
+export default App
---
+++ src/test.jsx
@@ -0,0 +1,9 @@
+import HomeCV from 'components/content/homecv'
+
+const Test = () => {
+    return (<>
+        <HomeCV />
+    </>)
+}
+
+export default Test
